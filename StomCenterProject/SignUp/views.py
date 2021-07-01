6503eacd1a6c08c81024f7b7a0a56d2018147c0e
bzhumakova <bzhumakova@gmail.com>
2021-07-01 18:39:17 +0600

    create views for OrderViewSet,OrderViewSet models and add functions to DayViewSet view.

--- StomCenterProject/SignUp/views.py
+++ StomCenterProject/SignUp/views.py
@@ -3,12 +3,114 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.viewsets import ViewSet
 from .serializers import *
+from .permissions import *
 
 class DayViewSet(ViewSet):
-    permission_classes = [IsAuthenticated]
+    permission_classes = [IsSuperUser]
     serializer_class=DaySerializer
 
     def list(self,request):
         days=Day.objects.all()
         serializer=self.serializer_class(days,many=True)
         return Response(serializer.data)
+
+    def create(self, request):
+        serializer = self.serializer_class(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=201)
+
+    def retrieve(self, request, day_id):
+        day = Day.objects.get(id=day_id)
+        serializer = self.serializer_class(day)
+        return Response(serializer.data, status=200)
+
+    def update(self, request, day_id):
+        day = Day.objects.get(id=day_id)
+        serializer = self.serializer_class(day, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=202)
+
+    def destroy(self, request, day_id):
+        day = Day.objects.get(id=day_id)
+        day.delete()
+        return Response(status=204)
+
+class DoctorDayView(ViewSet):
+    serializer_class = DoctorDaySerializer
+    permission_classes = [DoctorDayPermission]
+
+    def list(self, request):
+        doctor_days = DoctorDay.objects.filter(doctor=request.user)
+        serializer = DoctorDaySerializer(doctor_days, many=True)
+        return Response(serializer.data, status=200)
+
+    def create(self, request, day_id):
+        day = Day.objects.get(id=day_id)
+        work_day = DoctorDay.objects.create(doctor=request.user, day=day)
+        serializer = self.serializer_class(work_day)
+        return Response(serializer.data, status=201)
+
+    def update(self, request, doctor_day_id):
+        work_day = DoctorDay.objects.get(id=doctor_day_id)
+        serializer = DoctorDaySerializer(work_day, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=202)
+
+    def retrieve(self, request, doctor_day_id):
+        work_day = DoctorDay.objects.get(id=doctor_day_id)
+        serializer = DoctorDaySerializer(work_day)
+        return Response(serializer.data, status=200)
+
+class OrderViewSet(ViewSet):
+    permission_classes = [ClientPermission]
+    serializer_class=OrderSerializer
+    def create(self,request):
+        serializer=self.serializer_class(data=request.data)
+        client=request.user
+        if serializer.is_valid():
+            doctor_id=serializer.data.get('doctor')
+            day_id=serializer.data.get('day')
+            try:
+                work_day=DoctorDay.objects.get(doctor=doctor_id,day=day_id)
+                day=Day.objects.get(id=day_id)
+                doctor=User.objects.get(id=doctor_id)
+                if work_day.status=='free':
+                    order=Order.objects.create(client=client,doctor=doctor,day=day)
+                    work_day.status='reserced'
+                    work_day.save()
+                    return Response(serializer.data)
+                return Response('Это время уже занято!')
+            except DoctorDay.DoesNotExist:
+                return Response('Врач в такой день не работает!')
+        return Response(serializer.errors)
+    def list(self,request):
+        group=request.user.groups.all()[0].name
+        user=request.user
+        if group=='client':
+            orders=Order.objects.filter(client=user)
+        elif group=='doctor':
+            orders=Order.objects.filter(doctor=user)
+        serializer=OrderDisplaySerializer(orders,many=True)
+        return Response(serializer.data)
+
+    #TODO cannot update on day when doctor does not work
+    def update(self,request,order_id):
+        order=Order.objects.get(id=order_id)
+        serializer=self.serializer_class(order,data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data,status=202)
+        return Response(serializer.errors,status=400)
+    def retrieve(self,request,order_id):
+        order=Order.objects.get(id=order_id)
+        serializer=self.serializer_class(order)
+        return Response(serializer.data,status=200)
+
+    def destroy(self,request,order_id):
+        order=Order.objects.get(id=order_id)
+        o_id=order.id
+        order.delete()
+        return Response(f'{o_id} successfully deleted!',status=204)
