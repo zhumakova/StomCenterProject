c1dad8b211ac9a0d9cb2c4e7d1ebf8ec054826c0
bzhumakova <bzhumakova@gmail.com>
2021-07-01 18:35:04 +0600

    add Order model

--- StomCenterProject/SignUp/models.py
+++ StomCenterProject/SignUp/models.py
@@ -10,3 +10,9 @@ class Day(models.Model):
 class DoctorDay(models.Model):
     doctor=models.ForeignKey(User,on_delete=models.CASCADE)
     day=models.ForeignKey(Day,on_delete=models.CASCADE)
+
+class Order(models.Model):
+    client = models.ForeignKey(User,on_delete=models.CASCADE,related_name='clients')
+    day = models.ForeignKey(Day,on_delete=models.CASCADE)
+    doctor = models.ForeignKey(User,on_delete=models.CASCADE,related_name='doctors')
+    date_created = models.DateTimeField(auto_now_add=True)
