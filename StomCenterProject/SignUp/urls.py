103a2e84ddb7c0df48244c1c63d43a14b4184bb5
bzhumakova <bzhumakova@gmail.com>
2021-07-01 18:37:47 +0600

    create paths for DoctorDayView,OrderViewSet

--- StomCenterProject/SignUp/urls.py
+++ StomCenterProject/SignUp/urls.py
@@ -1,11 +1,37 @@
-from django.contrib import admin
-from django.urls import path,include
+from django.urls import path
+from .views import *
 
-from .views import DayViewSet
-
-urlpatterns=[
+urlpatterns = [
     path('',DayViewSet.as_view({
+        'get':'list',
+        'post':'create'
+                                }), name='day'),
+    path('day/<int:day_id>/',DayViewSet.as_view(
+        {
+            'get':'retrieve',
+            'put':'update',
+            'delete':'destroy',
+        }), name='day_detail'),
+    path('doctor_day/',DoctorDayView.as_view({
         'get':'list'
-    }))
+    }),name='doctor_day'),
+    path('create_wd/<int:day_id>/',DoctorDayView.as_view({
+        'post':'create'
+    }),name='create_wd'),
+    path('modify_wd/<int:doctor_day_id>/',DoctorDayView.as_view({
+        'put':'update',
+        'get':'retrieve',
+    }),name='modify_wd'),
+    path('order/',OrderViewSet.as_view({
+        'get':'list',
+        'post':'create',
+    }),name='order'),
+    path('order/<int:order_id>/',OrderViewSet.as_view({
+        'get':'retrieve',
+        'put':'update',
+        'delete':'destroy'
+    }
+    ))
+
 
 ]
