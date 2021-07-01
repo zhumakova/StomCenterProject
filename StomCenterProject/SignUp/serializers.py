69104fb887f11aeeaaea7c8533d7c00b92bb61d1
bzhumakova <bzhumakova@gmail.com>
2021-07-01 18:36:42 +0600

    add serializers for DoctorDaySerializer,DayDisplaySerializer,OrderSerializer,OrderDisplaySerializer models

--- StomCenterProject/SignUp/serializers.py
+++ StomCenterProject/SignUp/serializers.py
@@ -6,5 +6,36 @@ class DaySerializer(serializers.ModelSerializer):
     class Meta:
         model=Day
         fields='__all__'
+class DoctorDaySerializer(serializers.ModelSerializer):
 
+    class Meta:
+        model = DoctorDay
+        fields = '__all__'
+class DoctorDayDisplaySerializer(serializers.ModelSerializer):
+    doctor=serializers.StringRelatedField()
+    day=serializers.StringRelatedField()
 
+    class Meta:
+        model=DoctorDay
+        fields='__all__'
+class DayDisplaySerializer(serializers.ModelSerializer):
+    doctorday_set=DoctorDayDisplaySerializer(many=True,read_only=True)
+
+    class Meta:
+        model=DoctorDay
+        fields=['id','doctorday_set']
+
+class OrderSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model=Order
+        fields=['day','doctor']
+
+class OrderDisplaySerializer(serializers.ModelSerializer):
+    client=serializers.StringRelatedField()
+    day=serializers.StringRelatedField()
+    doctor=serializers.StringRelatedField()
+
+    class Meta:
+        model=Order
+        fields=['id','date_created','client','day','doctor']
